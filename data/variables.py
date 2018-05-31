d4ed63aaa14a4b0458fa760bfd74e2c6a102a37b
Galvayra <voltexlicer@gmail.com>
2018-05-31 14:37:14 +0900

    variables for option

--- data/variables.py
+++ data/variables.py
@@ -1,22 +1,22 @@
-import argparse
-
-parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
-
-
-def get_arguments():
-    parser.add_argument("-save", "--save", help="set dictionary file name\n"
-                                                "(default is 'dict')\n")
-    _args = parser.parse_args()
-
-    return _args
-
-
-args = get_arguments()
-
-if not args.save:
-    NAME_SAVE = "dict"
-else:
-    NAME_SAVE = args.dict
+# import argparse
+#
+# parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+#
+#
+# def get_arguments():
+#     parser.add_argument("-save", "--save", help="set dictionary file name\n"
+#                                                 "(default is 'dict')\n")
+#     _args = parser.parse_args()
+#
+#     return _args
+#
+#
+# args = get_arguments()
+#
+# if not args.save:
+#     NAME_SAVE = "dict"
+# else:
+#     NAME_SAVE = args.dict
 
 PATH_CORPUS = "corpus/"
 PATH_SAVE = "data/dict/"
