3d3f95a1a789fd0a16b5c74f4cda6f6952eb4116
Galvayra <voltexlicer@gmail.com>
2018-05-31 14:44:58 +0900

    build shell script

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 #/bin/bash
 
-dict=0
+corpus=0
 
 . utils/parse_options.sh || echo "Can't find parse_options.sh" | exit 1
 
-if [ $dict -eq 0 ]; then
-	dict="dict"
+if [ $corpus -eq 0 ]; then
+	corpus="corpus"
 fi
 
 echo
-echo "making dictionary named $dict in the data/dict/ directory !!"
+echo "making corpus named $corpus which is consist of sentences in all of corpus"
+echo "It is in the data/dict/ directory"
 echo
 
-python construct.py >> data/dict/$dict
+python construct.py >> data/dict/$corpus
 
 
