29c243bd8b4716ce3d59616d502c6c69c688aef0
Galvayra <voltexlicer@gmail.com>
2018-05-31 15:24:48 +0900

    merge all of sentences as a one file

--- merge.py
+++ merge.py
@@ -6,8 +6,8 @@ try:
 except ImportError:
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-from PosTagging.data.dataHandler import MyDataHandler
+from PosTagging.data.dataHandler import DataHandler
 
 if __name__ == '__main__':
-    data_handler = MyDataHandler()
+    data_handler = DataHandler()
     data_handler.print_sent_in_corpus()
