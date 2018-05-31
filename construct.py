0cf3d2e80d2ba58d3a5709ed2c84409371204acc
Galvayra <voltexlicer@gmail.com>
2018-05-31 15:25:15 +0900

    construct dictionary for POS Tagging

--- construct.py
+++ construct.py
@@ -6,8 +6,8 @@ try:
 except ImportError:
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-from PosTagging.data.dataHandler import MyDataHandler
+from PosTagging.data.dataHandler import DataHandler
 
 if __name__ == '__main__':
-    data_handler = MyDataHandler()
-    data_handler.construct_dict()
+    data_handler = DataHandler()
+    data_handler.set_dict4tag()
