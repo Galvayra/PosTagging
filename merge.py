2bda3ad5a4531e23214675cb2c747c3765474fb3
Galvayra <voltexlicer@gmail.com>
2018-05-31 14:50:54 +0900

    merge all of sentences in corpus

---
+++ merge.py
@@ -0,0 +1,13 @@
+import sys
+from os import path
+
+try:
+    import PosTagging
+except ImportError:
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+from PosTagging.data.dataHandler import MyDataHandler
+
+if __name__ == '__main__':
+    data_handler = MyDataHandler()
+    data_handler.print_sent_in_corpus()
