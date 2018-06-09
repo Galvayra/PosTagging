6884773adfdc919ec4feec23010bc1e34b33f19e
Galvayra <voltexlicer@gmail.com>
2018-06-10 02:57:46 +0900

    add comments in the HMM class

--- tagger/model.py
+++ tagger/model.py
@@ -29,14 +29,18 @@ class Hmm:
         else:
             return self.emission_map[tag][UNKNOWN_KEY]
 
-    def __get_weight_node(self, tag, pre_network, _emission_prob=0):
+    # when the tag is given, get node of weights as adding emission probability and previous node
+    # [word_1, ... , word_N]
+    # (from t to N) sigma(P(tag(t)|tag(t-1)) * P(word(t)|tag(t)))
+    def __get_weight_node(self, tag, pre_node, _emission_prob=0):
         transition_map = self.__get_transition_map(tag)
 
-        network = np.array([weight + transition_map[tag_given] for tag_given, weight in pre_network.items()])
-        network += _emission_prob
+        node = np.array([weight + transition_map[tag_given] for tag_given, weight in pre_node.items()])
+        node += _emission_prob
 
-        return network
+        return node
 
+    # get transition map only tag
     def __get_transition_map(self, tag, is_reverse=False):
         if is_reverse:
             return {key: value for key, value in self.transition_map[tag].items() if not key == END_FLAG}
@@ -46,49 +50,51 @@ class Hmm:
     # dynamic programming using viterbi
     def viterbi(self, observations):
 
-        # network == [{tag: prob, .... , tag: prob}, min_tag]
-        def __append_network__(_word, is_flag=False):
+        # node == [{tag: prob, .... , tag: prob}, min_tag]
+        def __append_node__(_word, is_flag=False):
             if is_flag:
                 words.append(_word)
             else:
                 answer.append(_word[1])
                 words.append(_word[0])
-                network.append([{tag: float() for tag in self.tags}, {tag: str() for tag in self.tags}])
+                node.append([{tag: float() for tag in self.tags}, {tag: str() for tag in self.tags}])
 
-        def __calculate_network__():
+        # calculate node during forward process
+        def __calculate_node__():
             target_word = words[index - 1]
 
             # process of END FLAG
             if target_word == END_FLAG:
-                current_network = self.__get_weight_node(target_word, network[index - 2][0])
-                network.append(self.tags[int(np.argmin(current_network))])
+                current_network = self.__get_weight_node(target_word, node[index - 2][0])
+                node.append(self.tags[int(np.argmin(current_network))])
             else:
-                target_network = network[index - 1]
-                for tag in target_network[0]:
+                target_node = node[index - 1]
+                for tag in target_node[0]:
                     emission_prob = self.__get_emission_prob(tag, target_word)
 
                     # if the word is occur first
                     # process of start word (word_1)
                     if index == INDEX_OF_FIRST_WORD:
                         transition_prob = self.transition_map[tag][START_FLAG]
-                        target_network[0][tag] = emission_prob + transition_prob
+                        target_node[0][tag] = emission_prob + transition_prob
 
                     # process of words (word_2, ... , word_N)
                     else:
-                        current_network = self.__get_weight_node(tag, network[index - 2][0], emission_prob)
-                        target_network[0][tag] = np.min(current_network)
-                        target_network[1][tag] = self.tags[int(np.argmin(current_network))]
+                        current_network = self.__get_weight_node(tag, node[index - 2][0], emission_prob)
+                        target_node[0][tag] = np.min(current_network)
+                        target_node[1][tag] = self.tags[int(np.argmin(current_network))]
 
+        # predict tags by back tracking
         def __back_tracking__():
-            predict.insert(0, network.pop())
+            predict.insert(0, node.pop())
 
-            for target_network in reversed(network):
+            for target_node in reversed(node):
                 tag = predict[-1]
-                predict.insert(0, target_network[1][tag])
+                predict.insert(0, target_node[1][tag])
 
             predict.pop(0)
 
-        network = list()
+        node = list()
         answer = list()
         words = list()
         predict = list()
@@ -104,25 +110,19 @@ class Hmm:
 
                     # process of word
                     if type(word) is tuple:
-                        __append_network__(word)
-                        __calculate_network__()
+                        __append_node__(word)
+                        __calculate_node__()
 
                     # process of END FLAG
                     elif type(word) is str and word == END_FLAG:
-                        __append_network__(word, is_flag=True)
-                        __calculate_network__()
+                        __append_node__(word, is_flag=True)
+                        __calculate_node__()
 
-        # back tracking
         __back_tracking__()
         print()
         print(predict)
         print(answer)
         print()
-
-        # print("\n\n========================\n\n")
-        # for i in network:
-        #     print()
-        #     print(i)
 
     def forward(self):
         pass
